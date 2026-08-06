3c88498456b4619ba02ee6d301e10a77754cb7ea
Tsundzukani-Angel <st10495904@rcconnect.edu.za>
2026-08-06 21:52:10 +0200

    Added Profile table

--- docs/RaceDay_Database.sql
+++ docs/RaceDay_Database.sql
@@ -1,5 +1,3 @@
-create database RaceDayDB;
-go
 use RaceDayDB;
 go
 -- Role table--
@@ -7,5 +5,22 @@ create table Role
 (
   RoleID int identity(1,1) primary key,
   RoleName varchar(20) not null unique
+);
+go
+
+--User table--
+create table [user]
+(
+  UserID int identity(1,1) primary key,
+  RoleID int not null,
+  FirstName varchar(50) not null,
+  LastName varchar(50) not null,
+  Email varchar(100) not null,
+  PasswordHash varchar(255) not null,
+  PhoneNumber varchar(20),
+  CreatedDate datetime default getdate(),
+  constraint fk_User_Role
+     foreign key(RoleID)
+     references Role(RoleID)
 );
 go
