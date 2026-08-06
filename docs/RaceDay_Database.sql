58d6cda1ad91b8017c529a0fc177a5e9e84fd3f8
Tsundzukani-Angel <st10495904@rcconnect.edu.za>
2026-08-06 22:44:19 +0200

    Added the Event and Category tables

--- docs/RaceDay_Database.sql
+++ docs/RaceDay_Database.sql
@@ -9,7 +9,7 @@ create table Role
 go
 
 --User table--
-create table [user]
+create table [User]
 (
   UserID int identity(1,1) primary key,
   RoleID int not null,
@@ -22,5 +22,60 @@ create table [user]
   constraint fk_User_Role
      foreign key(RoleID)
      references Role(RoleID)
+);
+go
+
+--Profile table--
+create table Profile
+(
+  ProfileID int identity(1,1) primary key,
+  UserID int not null unique, --unique becuase a user should only have one profile. (1:1)--
+  DateOfBirth Date,
+  Gender varchar(20),
+  EmergencyContact varchar(100),
+  MedicalNotes varchar(225),
+  constraint fk_Profile_User
+      foreign key (UserID)
+      references [User](UserID)
+);
+go
+
+--Event table--
+create table Event
+(
+  EventID int identity(1,1) primary key,
+  OrganiserID int not null,
+  Name varchar(100) not null,
+  Description varchar(500),
+  EventDate date not null,
+  Location varchar(100) not null,
+  Distance decimal(5,2) not null,
+  EventType varchar(20) not null,
+  MaxParticipants int not null,
+  RegistrationDeadline date not null,
+  Status varchar(20) not null default 'Open',
+  constraint fk_Event_Organiser
+     foreign key (OrganiserID)
+     references [User](UserID),
+  
+  constraint chk_EventType
+     check (EventType in ('Run','Walk','Cycle')),
+
+  constraint chk_EventStatus
+     check (Status in ('Open','Closed','Completed'))
+);
+go
+
+--Category table--
+create table Category
+(
+  CategoryID int identity(1,1) primary key,
+  EventID int not null,
+  CategoryName varchar(50) not null,
+  CategoryType varchar(20) not null,
+  EntryFee decimal(10,2) not null default 0,
+  constraint fk_Category_Event
+     foreign key (EventID)
+     references Event(EventID)
 );
 go
